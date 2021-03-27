9370e05f735fb42abdde63ad0521ad22a75645c5
cemarv25 <cesar.martinez0010@outlook.com>
2021-03-26 17:51:35 -0700

    Fixed comments

--- code.js
+++ code.js
@@ -1,11 +1,10 @@
-/**
- * Turns an array into a string, separating its elements with a comma.
- *
- * @param {Array} elements The array to be turned into string.
- * @returns {String} The elements of the array separated with a comma.
- */
 export default class ListToString {
-  // Array.join() returns a string of the elements of the array separated with a comma.
+  /**
+   * Turns an array into a string, separating its elements with a comma.
+   *
+   * @param {Array} elements The array to be turned into string.
+   * @returns {String} The elements of the array separated with a comma.
+   */
   concatenateList(elements) {
     return elements.join();
   }
