142f22eb6eb40f5a49abc79a4b51011e584fb979
cemarv25 <cesar.martinez0010@outlook.com>
2021-03-26 18:03:20 -0700

    Fixed export

--- code.js
+++ code.js
@@ -1,11 +1,14 @@
-export default class ListToString {
+class ListToString {
   /**
    * Turns an array into a string, separating its elements with a comma.
    *
    * @param {Array} elements The array to be turned into string.
-   * @returns {String} The elements of the array separated with a comma.
+   * @param {string} [separator = ,] The separator with which the string will separate the elements of the array.
+   * @returns {string} The elements of the array separated with a comma.
    */
-  concatenateList(elements) {
-    return elements.join();
+  concatenateList(elements, separator = ',') {
+    return elements.join(separator);
   }
 }
+
+module.exports = { ListToString };
